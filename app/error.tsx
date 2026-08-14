0929cfc6f6191c8696a8c222b7f5e7b633c37625
aklimaakhter <aklima00191@gmail.com>
2026-08-14 12:20:14 +0600

    error.tsx file setup

--- app/error.tsx
+++ app/error.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
-const error = () => {
+'use client' // Error boundaries must be Client Components
+ 
+import { useEffect } from 'react'
+ 
+export default function Error({
+  error,
+  retry,
+}: {
+  error: Error & { digest?: string }
+  retry: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+ 
   return (
-    <div>error</div>
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by re-fetching and re-rendering the segment
+          () => retry()
+        }
+      >
+        Try again
+      </button>
+    </div>
   )
 }
-
-export default error
